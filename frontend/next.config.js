663f4e90839178267d6e95a28a1417fdca018f71
DEVANTH2006-maker <devanth2006@gmail.com>
2026-08-28 00:32:32 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,21 @@
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+
   async rewrites() {
+
     return [
+
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
       },
+
     ];
+
   },
+
 };
 
 module.exports = nextConfig;
