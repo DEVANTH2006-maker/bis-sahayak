63a2e3f1192c946965fcf0e7f7b068d996b5dbc7
unknown <devanth2006@gmail.com>
2026-08-30 01:04:09 +0530

    Fix backend API rewrite URL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,14 @@
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
   async rewrites() {
-
     return [
-
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
+        destination: `${process.env.BACKEND_URL.replace(/\/+$/, '')}/api/:path*`,
       },
-
     ];
-
   },
-
 };
 
 module.exports = nextConfig;
